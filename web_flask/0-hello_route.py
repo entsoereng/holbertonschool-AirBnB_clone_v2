daa88c421bffcbdb0df71b17622649b8499644e9
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-04-09 16:16:07 +0200

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,12 @@
-# 0-hello_route.py
-
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello HBNB!"
 
-if __name__"__main__":
-    app.run(host="127.0.01", port=5000, debug=True)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
