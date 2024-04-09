e8c338e83060144ad64cb3d5b16666458c7b4ea1
entsoereng <entsoereng@aol.com>
2024-04-09 15:12:37 +0000

    flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,11 +2,10 @@ from flask import Flask
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
+
 @app.route("/")
 def hello():
     return "Hello HBNB!"
-=======
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -14,5 +13,4 @@ def hello_hbnb():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
->>>>>>> daa88c421bffcbdb0df71b17622649b8499644e9
+    app.run(host='0.0.0.0', port=5000, debug=True)
