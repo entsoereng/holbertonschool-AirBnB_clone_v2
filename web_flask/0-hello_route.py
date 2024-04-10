e861be8893df1671386b085fc3a8c6b542873d86
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-04-10 10:17:09 +0200

    Update 0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,14 @@
+#!/usr/bin/python3
+""" Start a Flask Web Application """
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello HBNB!"
-
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
+    """ Prints a Message """
     return 'Hello HBNB!'
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
