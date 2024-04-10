28d6020f11c25ea8589d9816a4dfb2624cdcb7fd
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-04-10 12:17:12 +0200

    Update 4-number_route.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,1 +1,40 @@
+#!/usr/bin/python3
+""" Starts a Flash Web Application HBNB"""
+from flask import Flask
+app = Flask(__name__)
 
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Prints a Message when / is called """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Prints a Message when /hbnb is called """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ Prints a Message when /c/<text> is called """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """ Prints a Message when /python/<text> is called """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """ Prints a Message when /number/<n> is called """
+    return '{} is a number'.format(n)
+
+
+if __name__ == "__main__":
+    """ Main Function """
+    app.run(host='0.0.0.0', port=5000)
