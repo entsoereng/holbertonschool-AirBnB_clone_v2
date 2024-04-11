37644d33abc84812c71b4dc27dddb7e05b08c8ff
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-04-11 12:55:12 +0200

    Update 8-cities_by_states.py

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,1 +1,30 @@
+#!/usr/bin/python3
+"""Script to start a Flask web application"""
 
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Close the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """Display a HTML page with the list of all cities grouped by states"""
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+
+    return render_template(
+        '8-cities_by_states.html',
+        states=sorted_states
+    )
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
