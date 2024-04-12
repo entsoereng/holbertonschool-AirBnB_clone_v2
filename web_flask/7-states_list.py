9d6182c0749b4d3cbaa9412d9cc144fc7358182b
Mokonyana Ntsoereng <entsoereng@aol.com>
2024-04-12 12:35:45 +0200

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -2,8 +2,8 @@
 """Script to start a Flask web application"""
 
 from flask import Flask, render_template
-from models import storage  # Import storage from models module
-from models.state import State  # Import State model
+from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
